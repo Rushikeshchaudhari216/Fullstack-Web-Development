fb3a338388ed572f09703c6a2bafd6e59c141472
Rushikesh Chaudhari <147311640+Rushikeshchaudhari216@users.noreply.github.com>
2025-07-26 13:02:22 +0530

    Add files via upload

---
+++ Javascript/57.JavaScript Loops/index.js
@@ -0,0 +1,35 @@
+let a = 1
+// for loop 
+for (let i = 0; i < 10; i++) {
+          console.log(a + i);
+}
+let object = {
+          name: "Harry",
+          role: "Programmer",
+          company: "CodeWithHarry AI"
+}
+
+// for in Loop //
+for (const key in object) {
+          console.log(key);
+}
+
+// for of loop //
+for (const c of "Rushikesh") {
+          console.log(c);
+
+}
+
+// while loop //
+let i = 0;
+while (i < 11) {
+          console.log(i);
+          i++;   
+}
+
+// do while loop //
+let i = 1;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
